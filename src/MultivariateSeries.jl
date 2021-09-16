eddbb9ed1b99486056cc87aa6c6faf1978e5b688
Benoît Legat <benoit.legat@gmail.com>
2021-09-16 11:39:20 +0200

    Avoid type piracy with DynamicPolynomials

--- src/MultivariateSeries.jl
+++ src/MultivariateSeries.jl
@@ -32,12 +32,4 @@ function DynamicPolynomials.MonomialVector(V::Vector{PolyVar{true}}, rg::Seq)
      L
 end
 
-function DynamicPolynomials.MonomialVector(V::Vector{PolyVar{true}}, rg::UnitRange{Int64})
-     L = DynamicPolynomials.Monomial{true}[]
-     for i in rg
-         append!(L, DynamicPolynomials.monomials(V,i))
-     end
-     L
-end
-
 end
